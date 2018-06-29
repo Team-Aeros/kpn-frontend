980533e18a139a910155632ea48f177a32d4f55d
TheElmo <jelmer.internet@xs4all.nl>
2018-06-29 17:06:25 +0200

    get mock data from service

--- src/app/kpn-api.service.ts
+++ src/app/kpn-api.service.ts
@@ -2,19 +2,147 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class KpnApiService {
 
-  constructor(private _http: HttpClient) { }
+    pricePlans;
+    discounts;
+    discountTypes;
+    constructor(private _http: HttpClient) {
+        this.fillPricePlans();
+        this.fillDiscounts();
+        this.fillDiscountTypes();
+    }
 
-  call(url: string, params: object) {
-      let data = {
-          params: params,
-      }
-      return this._http.post(environment.api_url + url, data, {headers: new HttpHeaders({ 'Content-Type': 'application/json' })});
-  }
+    fillPricePlans() {
+        this.pricePlans = [
+            {
+                id: 1,
+                name:'bert',
+                interval: 'monthly',
+                price: 20,
+                discount: 0,
+            },
+            {
+                id: 2,
+                name:'jelmer',
+                interval: 'monthly',
+                price: 50,
+                discount: 1,
+            },
+            {
+                id: 3,
+                name:'robert',
+                interval: 'monthly',
+                price: 30,
+                discount: 2,
+            },
+            {
+                id: 4,
+                name:'hinnie',
+                interval: 'monthly',
+                price: 60,
+                discount: 3,
+            },
+            {
+                id: 5,
+                name:'evert',
+                interval: 'monthly',
+                price: 10,
+                discount: 4,
+            },
+        ];
+    }
+
+    fillDiscounts() {
+        this.discounts = [
+            {
+                id: 0,
+                amount: 5,
+                typeName: 'volume',
+                typeId: 0
+            },
+            {
+                id: 1,
+                amount: 10,
+                typeName: 'product',
+                typeId: 0
+            },
+            {
+                id: 2,
+                amount: 5,
+                typeName: 'happyHour',
+                typeId: 0
+            },
+            {
+                id: 3,
+                amount: 5,
+                typeName: 'loyalty',
+                typeId: 0
+            },
+        ];
+    }
+
+    fillDiscountTypes() {
+        this.discountTypes = [
+            {
+                name: 'volume',
+                entries: [
+                    {
+                        id: 0,
+                        limit: 10,
+                        amount: 10,
+                        amountType: 'Gb'
+                    },
+                    {
+                        id: 1,
+                        limit: 5,
+                        amount: 5,
+                        amountType: 'Gb'
+                    },
+                ]
+            },
+        ];
+    }
+
+    call(url: string, params: object) {
+        let data = {
+            params: params,
+        }
+        return this._http.post(environment.api_url + url, data, {headers: new HttpHeaders({ 'Content-Type': 'application/json' })});
+    }
+
+    getAllPricePlanData() {
+        return this.pricePlans;
+    }
+
+    getPricePlanData(id: number) {
+        for(let pricePlan of this.pricePlans) {
+            if(pricePlan.id == id) {
+                return pricePlan;
+            }
+        }
+    }
+
+    getDiscount(id: number) {
+        for(let discount of this.discounts) {
+            if(discount.id == id) {
+                return discount;
+            }
+        }
+    }
+
+    getDiscountType(name: String, id: number) {
+        for(let discountType of this.discountTypes) {
+            if(discountType.name == name) {
+                for(let entry of discountType.entries) {
+                    if(entry.id == id) {
+                        return entry;
+                    }
+                } 
+            }
+        }
+    }
 
   // Zijn nog niet per se nodig!
   // unsafeGet(url: string) {
